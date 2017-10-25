e4a7bab827ab7d0c33c67a35d4c1ec62b5691e2c
SravaniKV <venkatasravanikaka@unomaha.edu>
2017-10-24 21:30:53 -0500

    Validator changes for fields

--- home/models.py
+++ home/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import Permission, User
+from django.core.validators import MinLengthValidator
+from django.core.validators import MaxLengthValidator
+from django.core.validators import RegexValidator
+from django.utils import timezone
 # Create your models here.
+
+regex=r'[0-9]'
 
 class Employee(models.Model):
     Employee_name = models.CharField(max_length=50)
-    Employee_phone = models.CharField(max_length=10)
-    #Employee_email = models.CharField(max_length=50)
+    Employee_phone = models.CharField(max_length=10, validators=[MinLengthValidator(10), RegexValidator(regex)],
+                                      help_text="Phone Number should be 10 digits")
+    Employee_email = models.EmailField(max_length=50, default='X@gmail.com')
     Employee_Address= models.CharField(max_length=500)
     Employee_Id= models.CharField(max_length=10)
 
@@ -14,11 +21,12 @@ class Employee(models.Model):
 
 
 class Mentor(models.Model):
-    #Mentor_email = models.CharField(max_length=50)
+    Mentor_email = models.EmailField(max_length=50, default='X@gmail.com')
     Mentor_name = models.CharField(max_length=50)
-    Mentor_phone = models.CharField(max_length=10)
+    Mentor_phone = models.CharField(max_length=10, validators=[MinLengthValidator(10), RegexValidator(regex)],
+                                    help_text="Phone Number should be 10 digits")
     Mentor_Address=models.CharField(max_length=500)
-    Mentor_Gender=models.CharField(max_length=1, default ='X')
+    Mentor_Gender=models.CharField(max_length=1, default ='X' ,help_text="Enter F or M")
     Mentor_Id=models.CharField(max_length=10)
 #    student_count = models.IntegerField(max_digits=10) (we should do the hardcode in later sprint)
 
@@ -28,10 +36,10 @@ class Mentor(models.Model):
 class Student(models.Model):
      Student_id= models.CharField(max_length=15)
      Student_name=models.CharField(max_length=50)
-     Student_email=models.CharField(max_length=25)
+     Student_email = models.EmailField(max_length=25, default='X@gmail.com')
      Student_grade = models.CharField(max_length=2, default='F')
-     Parents_email= models.CharField(max_length=25)
-     Parents_phone=models.CharField(max_length=15)
+     Parents_email = models.EmailField(max_length=25, default='X@gmail.com')
+     Parents_phone = models.CharField(validators=[MinLengthValidator(10), RegexValidator(regex)], max_length=10)
      School= models.CharField(max_length=50)
      Men_name =models.ForeignKey(Mentor,related_name='Menemail')
      Emp_name= models.ForeignKey(Employee, related_name='Empemail')
